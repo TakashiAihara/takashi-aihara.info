c67455fec33f3bf605f0036e43090b76f2521a90
TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
2023-07-25 01:34:29 +0000

    Close #8 --- Squashed commit of the following:
    commit 900f305b59a315cfcf1d05dcf45c5fe37d03bfe1
    Author: TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
    Date:   Tue Jul 25 01:34:21 2023 +0000
    
        add skills/bio/portfolio base
    
    commit 279a62faa1419de449ea3608c17b53ad793caaf8
    Author: TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
    Date:   Tue Jul 25 01:22:29 2023 +0000
    
        support sns link from consts
    
    commit 14a8a7ed25329563429f82174535401f5b8cdcf5
    Author: TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
    Date:   Tue Jul 25 01:22:03 2023 +0000
    
        rename site_repository_url
    
    commit 3375a4a490e6bdfdff2462dbdca3a5834aa2bd13
    Author: TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
    Date:   Tue Jul 25 00:47:53 2023 +0000
    
        change blog index order to desc
    
    commit 6cfc6f70e5ada558002a8f03256770efba83e28d
    Author: TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
    Date:   Tue Jul 25 00:40:08 2023 +0000
    
        change date format in blog
    
    commit 5cee182ccaf5ed18e1a34838cb95c8e0094cb32b
    Author: TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
    Date:   Tue Jul 25 00:38:50 2023 +0000
    
        extract astro repo url
    
    commit c43d806296876be576649e8812591fa2fae6d347
    Author: TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
    Date:   Tue Jul 25 00:36:55 2023 +0000
    
        modify Head for support slot
    
    commit 5090c2ea00f393b55a500341d74accc9be00bb06
    Author: TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
    Date:   Tue Jul 25 00:31:30 2023 +0000
    
        modify footer style
    
    commit 10ccd0c6cd27b3fc7506ebf20777109b14b598af
    Author: TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
    Date:   Tue Jul 25 00:19:53 2023 +0000
    
        modify footer
    
    commit 01ce54eb3ddeedb3b89459b9f39fae6dc2593e3d
    Author: TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
    Date:   Tue Jul 25 00:14:44 2023 +0000
    
        Rename BaseHead -> Head
    
    commit eab87a880a67d557264f9989b8d0e7f08516d4ab
    Author: TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
    Date:   Tue Jul 25 00:13:58 2023 +0000
    
        Modify Site basic

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,38 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "My personal website.";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+// Site Core Information
+export const SITE_TITLE = "About TakashiAihara";
+export const SITE_DESCRIPTION = "About TakashiAihara";
+export const SITE_REPOSITORY_URL = "https://github.com/TakashiAihara/takashi-aihara.info";
+export const ASTRO_REPOSITORY_URL = "https://github.com/withastro/astro";
+
+// SNS URLs
+export const TWITTER_URL = "https://twitter.com/takashi__aihara";
+export const GITHUB_URL = "https://github.com/TakashiAihara";
+export const ZENN_URL = "https://zenn.dev/takashiaihara";
+export const QIITA_URL = "https://qiita.com/TakashiAihara";
+
+// SNS header consts
+export const SNS_SETTINGS = [
+	{
+		name: "Twitter",
+		url: TWITTER_URL,
+		icon: "twitter"
+	},
+	{
+		name: "GitHub",
+		url: GITHUB_URL,
+		icon: "github"
+	},
+	{
+		name: "Zenn",
+		url: ZENN_URL,
+		icon: "zenn"
+	},
+	{
+		name: "Qiita",
+		url: QIITA_URL,
+		icon: "qiita"
+	}
+]
